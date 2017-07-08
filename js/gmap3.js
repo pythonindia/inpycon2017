5df396de03f0fa5f5afdfcb17ce9812879893c56
Rajat Saini <rajatvaidya14@outlook.com>
2017-07-08 15:30:12 +0530

    [v2] content, logo added (#15)
    * slider image added
    
    * Footer copyright added
    
    * logo added
    
    * logo added
    
    * files render after changes
    
    * content added and some sections are commented for future use
    
    * color theme changed
    
    * index file modified
    
    * sponsorship file added
    
    * jinja template added
    
    * Speaker annonymous image added
    
    * sponsor image added
    
    * coc file added
    
    * Pycon about section image added
    
    * template file modified
    
    * template file rendered
    
    * template file modified
    
    * map coordinates modified

--- js/gmap3.js
+++ js/gmap3.js
@@ -56,7 +56,7 @@ function initDefaults() {
       }()),
       map: {
         mapTypeId : gm.MapTypeId.ROADMAP,
-        center: [46.578498, 2.457275],
+        center: [28.6524018,77.0314263],
         zoom: 2
       },
       overlay: {
