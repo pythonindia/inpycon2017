9ca8ab01a63364ce6da62d58a08545277b633217
shashank7200 <shashank7200@gmail.com>
2017-07-15 15:32:37 +0530

    solves dead link formation after adding smooth scrolling

--- js/main.js
+++ js/main.js
@@ -52,9 +52,26 @@ jQuery(document).on('ready', function() {
 	/* -------------------------------------
 	     ADD ANIMATIONS TO SECTION SCROLL
 	 -------------------------------------- */
-    $('a[href*=\\#]').on('click', function(event){
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
+    function ea_scroll( hash ) {
+        var target = $( hash );
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            var top_offset = 0;
+            $('html,body').animate({
+                scrollTop: target.offset().top - top_offset
+            }, 1000);
+            return false;
+        }
+    }
+    // -- Smooth scroll on pageload
+    if( window.location.hash ) {
+        ea_scroll( window.location.hash );
+    }
+    // -- Smooth scroll on click
+    $('a[href*="#"]:not([href="#"]):not(.no-scroll)').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+            ea_scroll( this.hash );
+        }
     });
 
 	/* -------------------------------------
